fe9931ecd18b9c9133c272e4d3f5e3b05f464ac1
KateGordon21 <kate.gordon@mnsu.edu>
2023-10-21 10:44:07 -0400

    package priorities

--- packagePriorities.py
+++ packagePriorities.py
@@ -3,13 +3,46 @@ def packagePriority(n):
 
     for i in range(n):
         inputs = input().split(' ')
-        I.append(inputs[0])
-        A.append(inputs[1])
-        T.append(inputs[2])
-        S.append(inputs[3])
-        R.append(inputs[4])
+        I.append(int(inputs[0]))
+        A.append(int(inputs[1]))
+        T.append(int(inputs[2]))
+        S.append(int(inputs[3]))
+        R.append(int(inputs[4]))
 
-    numOfPackages
+    numOfPackages = n
+    packages = []
     order = []
 
+    while len(packages) < n:
+        if len(I):
 
+
+        RPriority = min(R)
+        SPriority = min(S)
+
+        minR = [I[i] for i in range(len(R)) if R[i] == RPriority]
+        minS = [minR[i] for i in range(len(minR)) if S[i] == SPriority]
+
+        if len(packages) == 1:
+            packages.append(minR[0])
+        elif len(minS) == 1:
+            packages.append(minS[0])
+        else:
+            minimumArrivalTime = 100001
+            minIndex = 0
+            for element in minS:
+                index = I.index(element)
+                if A[index] < minimumArrivalTime:
+                    minimumArrivalTime = A[index]
+                    minIndex = index
+            packages.append(I[minIndex])
+        for i in range(5):
+            print(I)
+            index = I.index(packages[-1])
+            I.pop(index)
+            A.pop(index)
+            R.pop(index)
+        return packages
+
+
+print(packagePriority(2))
